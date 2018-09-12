5f352aaf18deca8ca4ebf941c5c36a31b2d4e60e
jeffan15 <37373382+jeffan15@users.noreply.github.com>
2018-09-11 23:07:33 -0500

    hi

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int a=1000;
+    int suma=0;
+    for(int i=1;i<a;i++){
+        if(!(i%3)||!(i%5))
+            suma=suma+i;
+    }
+    cout<<suma<<endl;*/
+    /*int numero;
+    cout<<"Ingrese su numero: "<<endl;
+    cin>>numero;
+    (numero==1)?cout<<numero<<endl:cout<<1;
+    for(int cont=2;cont!=numero;++cont)
+        cout<<", "<<cont;*/
+
+    /*int numero;
+    cout<<"Ingrese su numero"<<endl;
+    cin>>numero;
+    for (int i=2; i<numero; i++)
+        for (int j=2; j<i; j++)
+        {
+            if (i % j == 0)
+                break;
+            else if (i == j+1)
+                cout << i << " ";
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
